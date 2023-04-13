6877b19adb374a6452aac8c98b0eab7ac4a8f81c
kirran01 <Kirran-@live.com>
2023-04-13 15:33:55 -0400

    getting data on the client

--- pages/transaction/page.js
+++ pages/transaction/page.js
@@ -8,12 +8,22 @@ export async function getServerSideProps() {
     },
   };
 }
-
 function TransactionPage({ data }) {
-  console.log(data, "transactionData");
+  let transactions = data.transaction;
+  console.log(transactions, "transactions");
   return (
     <div>
-      <h1>transactions</h1>
+      <h1>Transactions</h1>
+      <div>
+        
+      </div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>
+          <p>Amount: {transaction.amount}</p>
+          <p>Received By: {transaction.recievedBy.app_user.username}</p>
+          <p>Sent By:{transaction.sentBy.app_user.username}</p>
+        </div>
+      ))}
     </div>
   );
 }
