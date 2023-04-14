25687bcefbcc1a1738817cdbdea8b9a8ce0af401
kirran01 <Kirran-@live.com>
2023-04-14 12:14:20 -0400

    nav styles

--- components/nav.jsx
+++ components/nav.jsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 const Nav = () => {
     return (
         <nav className='bg-slate-800 text-white p-3 mb-3 flex justify-center lg:justify-start md:justify-start'>
-            <ul className='flex'>
-                <li className='mx-3 flex items-center'>
-                    <Link href="/">Home</Link>
-                </li>
-                <li className='mx-3'>
-                    <Link href="/user/page">Profile</Link>
-                </li>
-                <li className='mx-3'>
-                    <Link href="/transaction/page">Transactions</Link>
-                </li>
-            </ul>
-        </nav>
+        <ul className='flex'>
+            <li className='flex items-center mx-3 md:mx-1 lg:mx-3'>
+                <Link href="/">Home</Link>
+            </li>
+            <li className='flex items-center mx-3 md:mx-1 lg:mx-3'>
+                <Link href="/user/page">Profile</Link>
+            </li>
+            <li className='flex items-center mx-3 md:mx-1 lg:mx-3'>
+                <Link href="/transaction/page">Transactions</Link>
+            </li>
+        </ul>
+    </nav>
     );
 }
 
